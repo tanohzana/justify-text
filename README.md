2c36e8f631c69cde0fc8ab49709247cb58533271
Florian Adonis <tanohzana@gmail.com>
2020-12-14 20:14:20 +0000

    limit the words to the day

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Justify text
+
+A NPM package to justify text.
+
+## To run the project
+
+To run the project, create a `.env` file with the field `JWT_SECRET` and add a random string to it.
--- src/services/token/isRequestAuthorised.js
+++ src/services/token/isRequestAuthorised.js
@@ -1,15 +1,26 @@
 
 const REQUESTS_NUMBER = {};
-const WORDS_LIMIT = 800;
+const WORDS_LIMIT = 80000;
 
 const isRequestAuthorised = (token, string) => {
   if (REQUESTS_NUMBER[token]) {
-    REQUESTS_NUMBER[token]+= string.split(' ').length;
+    if (REQUESTS_NUMBER[token].lastModified < new Date().setHours(0,0,0,0)) {
+      REQUESTS_NUMBER[token] = {
+        count: 0,
+        lastModified: new Date(),
+      };
+    }
+
+    REQUESTS_NUMBER[token].count += string.split(' ').length;
+    REQUESTS_NUMBER[token].lastModified = new Date();
   } else {
-    REQUESTS_NUMBER[token] = string.split(' ').length;
+    REQUESTS_NUMBER[token] = {
+      count: string.split(' ').length,
+      lastModified: new Date(),
+    };
   }
 
-  return (REQUESTS_NUMBER[token] < WORDS_LIMIT);
+  return (REQUESTS_NUMBER[token].count < WORDS_LIMIT);
 };
 
 export default isRequestAuthorised;
